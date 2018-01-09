fc395f95a29cc0a2684c4ac4560f55cc006c09df
ScottHaag <smh362@drexel.edu>
2018-01-09 10:52:47 -0500

    addedintextfiles

---
+++ pg_sql/pythonnestedset.py
@@ -0,0 +1,15 @@
+import psycopg2; import numpy as np;
+
+try:
+    conn = psycopg2.connect("dbname='postgres' user='postgres' host='localhost' password='vn9jz9xr'")
+except:
+    print ( 'I am unable to connect to the database')
+    
+cur = conn.cursor();
+cur.execute("""select * from wikiwtershed.nhdplus_stream_nsidxprocess""");
+rows = np.matrix(cur.fetchall()).astype(int);
+
+stack = [1];
+
+def up(n,stack):
+    stack =    
