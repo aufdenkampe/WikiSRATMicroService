4bd401f0651810e62abe14393d40afd9a32a6de6
ScottHaag <smh362@drexel.edu>
2018-01-09 09:22:45 -0500

    fixedindexintablecreate

--- pg_sql/create_coefs_table.sql
+++ pg_sql/create_coefs_table.sql
@@ -8,13 +8,13 @@ Select * From wikiwtershed.nhdcoefs;
 
 set enable_seqscan = on;
 
-ALTER TABLE wikiwtershed.nhdcoefs
+ALTER TABLE wikiwtershed.cache_nhdcoefs
   ADD CONSTRAINT nhdpluscoefspk PRIMARY KEY(comid);
 
 CREATE INDEX 
-   ON wikiwtershed.nhdplus (huc12 ASC NULLS LAST);
+   ON wikiwtershed.cache_nhdcoefs (huc12 ASC NULLS LAST);
 
-ALTER TABLE wikiwtershed.nhdcoefs
+ALTER TABLE wikiwtershed.cache_nhdcoefs
   ADD FOREIGN KEY (huc12) REFERENCES wikiwtershed.boundary_huc12 (huc12)
    ON UPDATE NO ACTION ON DELETE NO ACTION;
 CREATE INDEX huc12foreignkeycoefs2
