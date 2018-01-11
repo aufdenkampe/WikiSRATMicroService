36cb8269e510665fdd100288e8b0b7cd3ecb18ee
mudkipmaster <acg59@drexel.edu>
2018-01-11 11:15:50 -0500

    added files I forgot to commit earlier

---
+++ DatabaseAdapter.py
@@ -0,0 +1,22 @@
+import psycopg2
+from constants import column_numbers
+
+
+class DatabaseAdapter:
+    def __init__(self, db_in, user_in, host_in, port_in, password_in):
+        try:
+            self.conn = psycopg2.connect(database=db_in, user=user_in, host=host_in, port=port_in, password=password_in)
+        except psycopg2.OperationalError as e:
+            raise AttributeError("unable to connect to datbase")
+
+    @classmethod
+    def python_to_array(self, python_object):
+        result = []
+        for _ in range(0, len(column_numbers)):  # create an array of arrays without numpy
+            result.append([])
+        for huc12 in python_object:
+            for attribute, value in huc12.items():
+                result[column_numbers[attribute]].append(value)
+        return result
+
+
