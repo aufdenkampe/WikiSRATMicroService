470b7933b17510a006b44a7ffac2bf7406f9fddd
ScottHaag <smh362@drexel.edu>
2018-01-25 11:09:19 -0500

    move_some_files_around

---
+++ pg_sql/process_Nutrient_grid.sql
@@ -0,0 +1,13 @@
+﻿-- File to Proces the Nutrient Grid Sent By barry Evans
+-- grid called natl_gwn
+-- merged with the stream catchment files using arcgis 
+-- from the NHDPlusv2 natiowide GIS file
+
+-- exported the original raster as 100 meter grid cells 100x100
+-- this is needed to get underneath the zonaal stastiics issues 
+
+
+-- # Replace a layer/table view name with a path to a dataset (which can be a layer file) or create the layer/table view within the script
+--# The following inputs are layers or table views: "Catchment", "natl_gwn"
+--arcpy.gp.ZonalStatisticsAsTable_sa("Catchment","FEATUREID","natl_gwn","C:/Users/smh362/Documents/ArcGIS/Default.gdb/ZonalSt_Catchme3","DATA","ALL")
+
