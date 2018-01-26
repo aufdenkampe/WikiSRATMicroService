1534d5e164bcc74d71a83054e339eb0bfff8558b
ScottHaag <smh362@drexel.edu>
2018-01-26 15:00:06 -0500

    add_nutrient_processing

--- pg_sql/process_Nutrient_grid.sql
+++ pg_sql/process_Nutrient_grid.sql
@@ -6,8 +6,39 @@
 -- exported the original raster as 100 meter grid cells 100x100
 -- this is needed to get underneath the zonaal stastiics issues 
 
+--# Replace a layer/table view name with a path to a dataset (which can be a layer file) or create the layer/table view within the script
+--# The following inputs are layers or table views: "Catchment", "natl_gwn_100x100.tif"
+--arcpy.gp.ZonalStatisticsAsTable_sa("Catchment","comid","natl_gwn_100x100.tif","E:/nhd_natltn_100x100","DATA","SUM")
 
--- # Replace a layer/table view name with a path to a dataset (which can be a layer file) or create the layer/table view within the script
---# The following inputs are layers or table views: "Catchment", "natl_gwn"
---arcpy.gp.ZonalStatisticsAsTable_sa("Catchment","FEATUREID","natl_gwn","C:/Users/smh362/Documents/ArcGIS/Default.gdb/ZonalSt_Catchme3","DATA","ALL")
+Drop Table If Exists wikiwtershed.grndwter_tn_nhdplus ;
+
+CREATE TABLE wikiwtershed.grndwter_tn_nhdplus 
+(
+rowid text,
+comid text,
+ZONE_CODE text,
+COUNT text, 
+AREA text,
+SUM text
+);
+
+
+COPY wikiwtershed.grndwter_tn_nhdplus 
+FROM '/home/drwi-user/nhd_natltn_1_26_18.csv' 
+WITH CSV HEADER DELIMITER AS ',';
+
+Alter Table wikiwtershed.grndwter_tn_nhdplus  Drop Column rowid;
+Alter Table wikiwtershed.grndwter_tn_nhdplus  Drop Column ZONE_CODE;
+Alter Table wikiwtershed.grndwter_tn_nhdplus  Drop Column COUNT;
+Alter Table wikiwtershed.grndwter_tn_nhdplus  Drop Column AREA;
+
+Select * From wikiwtershed.grndwter_tn_nhdplus limit 10
+
+ALTER TABLE wikiwtershed.grndwter_tn_nhdplus
+  ADD PRIMARY KEY (comid);
+
+
+Alter  Table wikiwtershed.grndwter_tn_nhdplus Alter Column SUM Type float using (replace(SUM, ',' ,'' ))::float;
+
+
 
